815a7a3fff0be7e17aa22f2be5b93de253e5fdeb
Alvin <192010003@smatalenta.sch.id>
2024-11-20 11:09:53 +0700

    tambahan

---
+++ RPLTubes/RPLTubes/src/test/java/com/example/RPLTubes/RplTubesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RPLTubes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RplTubesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
