54777ec2a21321f538756b71e7a7ba320fb2ff01
0fflines <6182201102@student.unpar.ac.id>
2024-12-08 13:40:05 +0700

    Tambah db tugas_akhir + BapController

--- data.sql
+++ data.sql
@@ -19,3 +19,14 @@ INSERT INTO skripsi_penguji (nama_mahasiswa, judul_skripsi, waktu, tempat, nilai
 ('Wombat', 'Skripsi A', '2022-09-10 13:00:00', 'Ruang D', 76, '2022/2023'),
 ('Kapibara', 'Skripsi B', '2023-02-8 14:00:00', 'Ruang B', 86, '2022/2023'),
 ('KapiDion', 'Skripsi B', '2020-02-8 16:00:00', 'Ruang D', 76, '2020/2021');
+
+--buat bagian siapa aja penguji sama pembibing nyimpennya gimana belum kepikir sori
+CREATE TABLE tugas_akhir(
+    id SERIAL PRIMARY KEY,
+    nama_mahasiswa VARCHAR(100) NOT NULL,
+    judul_skripsi VARCHAR(100),
+    waktu_sidang TIMESTAMP,
+    tempat_sidang VARCHAR(100),
+    tipe_sidang INTEGER,
+    bap_file BYTEA NOT NULL
+)
